1a2fc302ebc95e44c42a5bad23c17a2a35b23670
Yirmeyahuw Allen <yirme.allen@berkeley.edu>
2023-10-29 10:48:34 -0700

    dummy data add

--- antitilt.py
+++ antitilt.py
@@ -7,6 +7,7 @@ import time
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
+import random
 
 
 
@@ -18,6 +19,14 @@ connection_params = {
     'port' : 16376
 }
 
+all_emotions = ['Admiration', 'Adoration','Aesthetic Appreciation','Amusement','Anger','Anxiety','Awe','Awkwardness',
+                'Boredom','Calmness','Concentration','Confusion','Contemplation','Contempt','Contentment','Craving',
+                'Desire','Determination','Disappointment','Disgust','Distress','Doubt','Ecstasy','Embarrassment',
+                'Empathic Pain','Entrancement','Envy','Excitement','Fear','Guilt','Horror','Interest',
+                'Joy','Love','Nostalgia','Pain','Pride','Realization','Relief','Romance',
+                'Sadness','Satisfaction','Shame','Surprise (negative)','Surprise (positive)','Sympathy','Tiredness','Triumph']
+
+main_table = "Golden_Touch"
 server = mindsdb_sdk.connect()
 
 dt = datetime
@@ -40,18 +49,18 @@ def create_crypto_tables():
         # List of cryptocurrencies to create tables for
         currencies
 
-        for crypto in currencies:
-            # SQL query to create tables
-            create_table_query = f'''
-            CREATE TABLE IF NOT EXISTS {crypto} (
-                emotion VARCHAR(50),
-                weight FLOAT,
-                time_of_trade timestamp PRIMARY KEY,
-                category VARCHAR(155)
-            );
-            '''
-            cursor.execute(create_table_query)
-            print(f"Table '{crypto}' created successfully.")
+        
+        create_table_query = f'''
+        CREATE TABLE IF NOT EXISTS {main_table} (
+            currency VARCHAR(50),
+            emotion VARCHAR(50),
+            weight FLOAT,
+            time_of_trade timestamp PRIMARY KEY,
+            category VARCHAR(200)
+        );
+        '''
+        cursor.execute(create_table_query)
+        print("Table created successfully.")
 
         # Commit the changes
         conn.commit()
@@ -64,7 +73,7 @@ def create_crypto_tables():
         print("Error while connecting to PostgreSQL", error)
 
 
-def filter_table_by_emotion(emotion_name, table):
+def filter_table_by_emotion(emotion_name, currency):
     try:
         # Establish a connection to the PostgreSQL server
         conn = psycopg2.connect(
@@ -75,7 +84,7 @@ def filter_table_by_emotion(emotion_name, table):
         cursor = conn.cursor()
 
         # SQL query to fetch records filtered by emotion
-        filter_query = f"SELECT * FROM {table} WHERE emotion = %s"
+        filter_query = f"SELECT * FROM {main_table} WHERE emotion = %s"
         cursor.execute(filter_query, (emotion_name, ))
 
         # Fetch all records with the specified emotion in the 'emotions' column
@@ -141,7 +150,7 @@ def get_crypto(crypto_name):
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
         
-def insert_into_table(currency, emotion, weight, category, time=datetime.datetime.now()):
+def insert_into_table(currency, emotion, weight, category, time=None):
     try:
         # Establish a connection to the PostgreSQL server
         conn = psycopg2.connect(
@@ -151,9 +160,12 @@ def insert_into_table(currency, emotion, weight, category, time=datetime.datetim
         # Create a cursor
         cursor = conn.cursor()
 
+        if time == None:
+            time = datetime.datetime.now()
+
         # SQL query to insert values into the specified table
-        insert_query = f"INSERT INTO {currency} (emotion, weight, time_of_trade, category) VALUES (%s, %s, %s, %s);"
-        cursor.execute(insert_query, (emotion, weight, time.strftime(f'%Y-%m-%d %H:%M:%S.%f'), category))
+        insert_query = f"INSERT INTO {main_table} (currency, emotion, weight, time_of_trade, category) VALUES (%s, %s, %s, %s, %s);"
+        cursor.execute(insert_query, (currency, emotion, weight, time.strftime(f'%Y-%m-%d %H:%M:%S.%f'), category))
 
         # Commit the changes
         conn.commit()
@@ -173,28 +185,28 @@ def to_df():
     )
     
     for curr in currencies:
-        query = f"SELECT * FROM {curr};"
+        query = f"SELECT * FROM {main_table};"
         dataframes[curr] = pd.read_sql_query(query, conn, parse_dates=['datetime'])
 
+def create_dummy_data():
+    for curr in currencies:
+        for i in range(25):
+            emotion = np.random.choice(all_emotions)
+            mean = random.random()
+            percentage = random.random()
+            if mean >= 0.75 and mean <= 1:
+                description = f"This trade was characterized by very prominent {emotion}, which was observed {np.round(percentage * 10, decimals=2)}% of the time with a mean expressiveness score of {np.round(mean, decimals=2)}."
+            elif mean >= 0.5 and mean < 0.75:
+                description = f"{emotion} was fairly noticeable {np.round(percentage * 10, decimals=2)}% of the time elapsed for this trade with a mean expressiveness score of {np.round(mean, decimals=2)}."
+            elif mean >= 0.3 and mean < 0.5:
+                description = f"Some {emotion} was detected during {np.round(percentage * 10, decimals=2)}% of this trade with a mean expressiveness score of {np.round(mean, decimals=2)}."
+            elif mean < 0.3:
+                description = "This trade had no significant emotional context."
+            insert_into_table(curr, emotion, np.round(mean, decimals=2), description, time=None)
+            time.sleep(2)
+            
+
+            
 drop_all_tables()
 create_crypto_tables()
-
-insert_into_table(
-    'BTCUSDT',
-    'anger',
-    .0031,
-    'anger is not prominent .31 percent of the time '
-    )
-
-time.sleep(2)
-
-insert_into_table(
-    'BTCUSDT',
-    'anger',
-    .12341,
-    'anger is very prominent 80% of the time '
-    )
-
-filter_table_by_emotion(
-    'anger', 'BTCUSDT'
-)
+create_dummy_data()
