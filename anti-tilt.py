3c0f4daf5143fd0049c11df340f7e9883974a7d5
Yirmeyahuw Allen <yirme.allen@berkeley.edu>
2023-10-29 02:12:37 -0700

    insert function implemented

--- anti-tilt.py
+++ anti-tilt.py
@@ -1,157 +1,175 @@
 import json 
 import psycopg2
 from psycopg2 import sql 
-import datetime as dt 
+import datetime 
 import mindsdb_sdk
 
 
-def main():
-    connection_params = {
-        'database': 'Midas', 
-        'user': 'postgres',
-        'password' : 'password', 
-        'host' : '6.tcp.us-cal-1.ngrok.io',
-        'port' : 16376
-    }
-    
-    server = mindsdb_sdk.connect()
-    
-    def create_crypto_tables():
-        try:
-            # Establish a connection to the PostgreSQL server
-            conn = psycopg2.connect(
-                **connection_params
-            )
 
-            # Create a cursor
-            cursor = conn.cursor()
+connection_params = {
+    'database': 'Midas', 
+    'user': 'postgres',
+    'password' : 'password', 
+    'host' : '6.tcp.us-cal-1.ngrok.io',
+    'port' : 16376
+}
 
-            # List of cryptocurrencies to create tables for
-            cryptocurrencies = ['btc', 'eth', 'xrp', 'doge']
+server = mindsdb_sdk.connect()
 
-            for crypto in cryptocurrencies:
-                # SQL query to create tables
-                create_table_query = f'''
-                CREATE TABLE IF NOT EXISTS {crypto} (
-                    emotions VARCHAR(50),
-                    delta FLOAT,
-                    timestamp TIMESTAMPTZ PRIMARY KEY,
-                    delta FLOAT
-                );
-                '''
-                cursor.execute(create_table_query)
-                print(f"Table '{crypto}' created successfully.")
+dt = datetime
 
-            # Commit the changes
-            conn.commit()
 
-            # Close the cursor and connection
-            cursor.close()
-            conn.close()
+def create_crypto_tables():
+    try:
+        # Establish a connection to the PostgreSQL server
+        conn = psycopg2.connect(
+            **connection_params
+        )
 
-        except (Exception, psycopg2.Error) as error:
-            print("Error while connecting to PostgreSQL", error)
-    
+        # Create a cursor
+        cursor = conn.cursor()
 
-    def filter_emoton_threshold(emotion, threshold_value):
-        
-        try:
-            # Connect to the PostgreSQL database
-            connection = psycopg2.connect(
-                **connection_params
-            )
+        # List of cryptocurrencies to create tables for
+        cryptocurrencies = ['btc', 'eth', 'xrp', 'doge']
 
-            cursor = connection.cursor()
+        for crypto in cryptocurrencies:
+            # SQL query to create tables
+            create_table_query = f'''
+            CREATE TABLE IF NOT EXISTS {crypto} (
+                emotion VARCHAR(50),
+                weight FLOAT,
+                timestamp TIMESTAMPTZ PRIMARY KEY,
+                category VARCHAR(50),
+                delta FLOAT
+            );
+            '''
+            cursor.execute(create_table_query)
+            print(f"Table '{crypto}' created successfully.")
 
-            # Construct the SQL query using the provided column and threshold value
-            query = sql.SQL("SELECT * FROM your_table WHERE {} > %s").format(sql.Identifier(emotion))
-            
-            # Execute the query with the threshold value as a parameter
-            cursor.execute(query, (threshold_value,))
+        # Commit the changes
+        conn.commit()
 
-            # Fetch and print the results
-            rows = cursor.fetchall()
-            for row in rows:
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
+
+def filter_table_by_emotion(emotion_name, table):
+    try:
+        # Establish a connection to the PostgreSQL server
+        conn = psycopg2.connect(
+            **connection_params
+        )
+
+        # Create a cursor
+        cursor = conn.cursor()
+
+        # SQL query to fetch records filtered by emotion
+        filter_query = "SELECT * FROM {table}} WHERE emotions = {emotion_name};"
+        cursor.execute(filter_query, emotion_name)
+
+        # Fetch all records with the specified emotion in the 'emotions' column
+        filtered_data = cursor.fetchall()
+
+        # Print or process the filtered data
+        if filtered_data:
+            print(f"Records with emotion '{emotion_name}':")
+            for row in filtered_data:
                 print(row)
+            return filtered_data
+        else:
+            print(f"No records found with emotion '{emotion_name}'")
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
     
-            # Close the cursor and connection
-            cursor.close()
-            connection.close()
 
-        except psycopg2.Error as e:
-            print("Error connecting to PostgreSQL:", e)
-    
-    def get_crypto(database_name):
-        try:
-            # Establish a connection to the PostgreSQL server
-            connection = psycopg2.connect(
-                **connection_params
-            )
-            # Create a cursor
-            cursor = connection.cursor()
+def drop_all_tables():
+    try:
+        # Establish a connection to the PostgreSQL server
+        conn = psycopg2.connect(
+        **connection_params
+    )
 
-            # SQL query to check for a database by name
-            query = sql.SQL("SELECT datname FROM pg_database WHERE datname = %s;")
-            cursor.execute(query, (database_name,))
+        # Create a cursor
+        cursor = conn.cursor()
 
-            # Fetch the results
-            database = cursor.fetchone()
+        # SQL query to retrieve all table names
+        get_table_names_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';"
+        cursor.execute(get_table_names_query)
+        
+        # Fetch all table names
+        tables = cursor.fetchall()
 
-            if database:
-                print(f"Database '{database_name}' exists.")
-                return database
-            else:
-                print(f"Database '{database_name}' does not exist.")
-                return None
+        # Drop each table
+        for table in tables:
+            drop_table_query = f"DROP TABLE {table[0]} CASCADE;"
+            cursor.execute(drop_table_query)
+            print(f"Dropped table: {table[0]}")
 
-            # Close the cursor and connection
-            cursor.close()
-            connection.close()
+        # Commit the changes
+        conn.commit()
 
-        except (Exception, psycopg2.Error) as error:
-            print("Error while connecting to PostgreSQL: ", error)
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
             
-        def drop_all_tables():
-            try:
-                # Establish a connection to the PostgreSQL server
-                conn = psycopg2.connect(
-                **connection_params
-            )
+def get_crypto(crypto_name):
+    try: 
+        conn = psycopg2.connect(
+            **connection_params
+        )
+        
+        cursor = conn.cursor()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+        
+def insert_into_table(currency, emotion, weight, category):
+    try:
+        # Establish a connection to the PostgreSQL server
+        conn = psycopg2.connect(
+            **connection_params
+        )
 
-                # Create a cursor
-                cursor = conn.cursor()
+        # Create a cursor
+        cursor = conn.cursor()
+        
+        time = datetime.datetime.now()
 
-                # SQL query to retrieve all table names
-                get_table_names_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';"
-                cursor.execute(get_table_names_query)
-                
-                # Fetch all table names
-                tables = cursor.fetchall()
+        # SQL query to insert values into the specified table
+        insert_query = f"INSERT INTO {currency} (emotion, weight, timestamp, category) VALUES (%s, %s, %s, %s);"
+        cursor.execute(insert_query, (emotion, weight, time, category))
 
-                # Drop each table
-                for table in tables:
-                    drop_table_query = f"DROP TABLE {table[0]} CASCADE;"
-                    cursor.execute(drop_table_query)
-                    print(f"Dropped table: {table[0]}")
+        # Commit the changes
+        conn.commit()
+        print("Values inserted into the table successfully.")
 
-                # Commit the changes
-                conn.commit()
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
 
-                # Close the cursor and connection
-                cursor.close()
-                conn.close()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
 
-            except (Exception, psycopg2.Error) as error:
-                print("Error while connecting to PostgreSQL", error)
+        
+drop_all_tables()
+create_crypto_tables()
 
-    create_crypto_tables()
-    
-    # Replace these values with your PostgreSQL credentials
-    database_name_to_find = "Midas"
+insert_into_table(
+    'btc',
+    'anger',
+    .0031,
+    'very'
+    )
 
-    # Call the function with the database name you want to find
-    get_crypto(database_name_to_find)
-    
-    
-            
-main()
+
