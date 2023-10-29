6b5209e111287292500e86cab1223637ac35dc7a
Luan da Cruz <luan.cruz@berkeley.edu>
2023-10-29 08:59:14 -0700

    Finished emotion predictor, need to wrap as function

--- facial_expressions.py
+++ facial_expressions.py
@@ -3,12 +3,9 @@ import pandas as pd
 
 import json
 
-import asyncio
-
-from hume import HumeStreamClient, StreamSocket, HumeBatchClient
+from hume import HumeBatchClient
 from hume.models.config import FaceConfig
 
-import os
 import cv2
 
 LUAN_API_KEY = "0BU3yyyBmwT7r6uXV8uh7q17pjNQofKCUnDA2ZfNAGl5Rn8U"
@@ -24,7 +21,7 @@ all_emotions = ['Admiration', 'Adoration','Aesthetic Appreciation','Amusement','
 webcam = cv2.VideoCapture(0)
 
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-out = cv2.VideoWriter('tradevid.mp4', fourcc, 30.0, (640, 480)) # 30 fps, 640 x 480 px
+out = cv2.VideoWriter('tradefootage.mp4', fourcc, 30.0, (640, 480)) # 30 fps, 640 x 480 px
 
 while(True):
     # Capture frame
@@ -32,10 +29,10 @@ while(True):
 
     # Check for successful capture
     if check:
-        # Write the frame into the file 'tradevid.mp4'
+        # Write the frame into the file 'tradefootage.mp4'
         out.write(frame)
         # Display the resulting frame
-        cv2.imshow("IronMind", frame)
+        cv2.imshow("Midas", frame)
 
     # Stop recording when 'esc' key is pressed
     if cv2.waitKey(1) & 0xFF == 27:
@@ -50,36 +47,49 @@ cv2.destroyAllWindows()
 batch_client = HumeBatchClient(LUAN_API_KEY)
 
 # Send video to analyze facial expressions
-job = batch_client.submit_job(urls=[], configs=[FaceConfig(identify_faces=True)], files=["tradevid.mp4"])
+job = batch_client.submit_job(urls=[], configs=[FaceConfig(identify_faces=True)], files=["tradefootage.mp4"])
 details = job.await_complete()
 job.download_predictions("trade_emotions.json")
 
-# Process emotion predictions into pandas DataFrame with mean score per emotion
+# Process emotion predictions into pandas DataFrame
 vid_dict = json.load(open("trade_emotions.json"))[0]["results"]["predictions"][0]["models"]["face"]["grouped_predictions"][0]["predictions"]
-trade_df = pd.DataFrame.from_dict({"name": all_emotions, "score": np.repeat(0.0, 48)})
+trade_df = pd.DataFrame.from_dict({"name": all_emotions, "score": np.repeat(0.0, 48),
+                                   "top count": np.repeat(0, 48), "total top score": np.repeat(0.0, 48)})
 num_frames = len(vid_dict)
 for i in range(num_frames):
     emotions_dict = vid_dict[i]["emotions"]
     emotions_df = pd.DataFrame.from_dict(emotions_dict).sort_values("name")
     trade_df["score"] = trade_df["score"] + emotions_df["score"]
+
+    emotions_df = emotions_df.sort_values("score", ascending=False)
+    index = emotions_df.iloc[[0]].index[0]
+    trade_df.iloc[index, 2] += 1
+    trade_df.iloc[index, 3] += emotions_df.iloc[0, 1]
 trade_df["score"] = trade_df["score"] / num_frames
+trade_df.rename(columns={"score": "mean score"}, inplace=True)
+trade_df["mean top score"] = (trade_df["total top score"] / trade_df["top count"]).fillna(0.0)
 
-# Identify emotion with highest mean value
-trade_df = trade_df.sort_values("score", ascending=False)
-print(trade_df)
-top_emotion = trade_df.iloc[0, 0]
-print(f"Top emotion: {top_emotion}")
-top_score = trade_df.iloc[0, 1]
-print(f"Score for top emotion: {top_score}")
+# Define statistics
+highest_mean = trade_df.sort_values("mean score", ascending=False).iloc[0, 0], trade_df.sort_values("mean score", ascending=False).iloc[0, 1]
+top = trade_df[trade_df["top count"] > 0].sort_values("top count", ascending=False)
+top_emotion = top.iloc[0, 0]
+top_emotion_time_proportion = top.iloc[0, 2] / num_frames
+top_emotion_mean_score = round(top.iloc[0, 4], 2)
 
-# Decide on output message depending on emotion value
+# Decide on descriptive message depending on emotion score
 description = ""
-if top_score >= 0.75 and top_score <= 1:
-    description = f"This trade was characterized by very prominent ({np.round(top_score * 100, 2)}%) {top_emotion}."
-elif top_score >= 0.5 and top_score < 0.75:
-    description = f"{top_emotion} was noticeable ({np.round(top_score * 100, 2)}%) throughout this trade."
-elif top_score >= 0.3 and top_score < 0.5:
-    description = f"Some {top_emotion} was detected ({np.round(top_score * 100, 2)}%) during this trade."
-elif top_score < 0.3:
-    description = "No significant emotional data for this trade."
-print(description)
+percentage = round(top_emotion_time_proportion * 100, 1)
+
+if top_emotion_mean_score >= 0.75 and top_emotion_mean_score <= 1:
+    description = f"This trade was characterized by very prominent {top_emotion}, which was observed {percentage}% of the time with a mean expressiveness score of {top_emotion_mean_score}."
+elif top_emotion_mean_score >= 0.5 and top_emotion_mean_score < 0.75:
+    description = f"{top_emotion} was fairly noticeable {percentage}% of the time elapsed for this trade with a mean expressiveness score of {top_emotion_mean_score}."
+elif top_emotion_mean_score >= 0.3 and top_emotion_mean_score < 0.5:
+    description = f"Some {top_emotion} was detected during {percentage}% of this trade with a mean expressiveness score of {top_emotion_mean_score}."
+elif top_emotion_mean_score < 0.3:
+    description = "This trade had no significant emotional context."
+
+# Test printing
+# print(top)
+# print(f"Using mean over entire vid: {highest_mean}")
+# print(description)
